69b947ac423dc17970f3fc99f2be864ead7270ad
muktarkabir <mukhtarkabir3394@gmail.com>
2025-09-07 03:16:44 +0100

    Write code for model classes.

---
+++ models/linked-list.js
@@ -0,0 +1,15 @@
+class LinkedList {
+ nodes = [];   
+  append(value){}
+  prepend(value){}
+  size(){}
+  head(){}
+  tail(){};
+  at(){};
+  pop(){};
+  contains(){};
+  find(){};
+  toString(){};
+  insertAt(value,index){};
+  removeAt(index){};
+}
