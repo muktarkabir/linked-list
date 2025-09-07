0dd4912a84a6da5108c4c2097f39f83c7f746285
mukhtarkabir <mukhtarkabir3394@gmail.com>
2025-09-08 00:04:11 +0100

    Add apend method to linked list

--- models/linked-list.js
+++ models/linked-list.js
@@ -1,6 +1,20 @@
 class LinkedList {
- nodes = [];   
-  append(value){}
+  constructor(head = null) {
+    this.head = head;
+  }
+  size = 0;
+  append(value) {
+    if (!this.head) {
+      this.head = new Node(value);
+    } else {
+      let temp = this.head;
+      while (temp.nextNode != null) {
+        temp = temp.nextNode;
+      }
+      temp.nextNode = new Node(value);
+    }
+    this.size++;
+  }
   prepend(value){}
   size(){}
   head(){}
