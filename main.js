ca623df9425431819f706da44ae696fd9ea55c75
mukhtarkabir <mukhtarkabir3394@gmail.com>
2025-09-08 07:50:12 +0100

    Test linkedList methods.

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ import LinkedList from "./models/linked-list.js";
 
 const list = new LinkedList();
 list.append("fish");
+list.prepend("Tortoise");
+list.append("dragonfly");
 console.log(list.toString());
+console.log(list.tail());
